625566408d2ddb2939aa141a1d487e57e9b01814
Mario Pereira <mariocezarpjr@gmail.com>
2018-07-07 22:18:39 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 var index = require("../index.js")
 
 
-describe('Name', () => {
+describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
